6adb2e874b906a18efa0f73259437c8cfb19b84c
nikhil batta <nbatta08@gmail.com>
2019-08-30 09:09:49 -0700

    writing logic out in pseduo code to brainstorm

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+function Pizza(amount,toppings,size){
+  this.amount = ammount,
+  this.toppings = toppings,
+  this.size = size
+}
+Pizza.prototype.addUpPrice = function(){
+  this.price = //amount of topings value plus amount of pizza
+  // amount of toppings + amountofpizza times baseprice + size
+}
+function Customer (amountOfPizza){
+  this.amountOfPizza = ammountOfPizza
+  this.price = 0
+}
+
+$(document).ready(function(){
+  var amountGrabber = //user inputted value
+  var toppingGrabber = //user inputted check
+  var sizeGrabber = // same thing
+  var newCustomer = new Customer()
+})
