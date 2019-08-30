e573b12b1d6ffb9270c93d0b90d29a6373d798cf
nikhil batta <nbatta08@gmail.com>
2019-08-30 13:26:57 -0700

    figured out the logic for adding together total price and parseinting values

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,15 @@
-function Pizza(amount,toppings,size){
+function Pizza(amount,toppings,size,totalprice){
   this.amount = amount,
   this.toppings = toppings,
-  this.size = size
+  this.size = size,
+  this.baseprice = 5;
+  this.totalprice = totalprice;
 }
-// Pizza.prototype.addUpPrice = function(){
-//   this.price = //amount of topings value plus amount of pizza
-//   // amount of toppings + amountofpizza times baseprice + size
-// }
+Pizza.prototype.addUpPrice = function(){
+  this.totalprice = (this.baseprice * this.amount) + this.toppings + this.size
+  console.log(this.price) //amount of topings value plus amount of pizza
+  // amount of toppings + amountofpizza times baseprice + size
+}
 function Customer (){
   this.price = 0;
 }
@@ -18,14 +21,15 @@ $(document).ready(function(){
   $('.toppings:checked').each(function() {
        toppingGrabber+=1
    });
-  var amountGrabber = $("#amount").val() //user inputted value
+  var amountGrabber = parseInt($("#amount").val()); //user inputted value
   //user inputted check
-  var sizeGrabber = $("#size").val()// same thing
+  var sizeGrabber = parseInt($("#size").val());// same thing
   console.log(amountGrabber)
   console.log(toppingGrabber)
   console.log(sizeGrabber)
   var newCustomer = new Customer()
-  var newPizza = new Pizza(amountGrabber,toppingGrabber,sizeGrabber)
+  var newPizza = new Pizza(amountGrabber,toppingGrabber,sizeGrabber,newCustomer)
+  newPizza.addUpPrice();
   console.log(newPizza)
 });
 });
