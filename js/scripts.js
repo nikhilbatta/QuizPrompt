1c39d590f6efbc554a2aae9862e9b9d8bb7ff31c
nbatta08@gmail.com <example@example.com>
2019-09-07 13:54:43 -0700

    figured out what my problem was for displaying, i think

--- js/scripts.js
+++ js/scripts.js
@@ -6,13 +6,9 @@ function Pizza(toppings,size){
 Pizza.prototype.getToppingCount = function() {
   return this.toppings.length;
 }
-// Pizza.prototype.addOrders = function(){
-//   this.items = []
-// }
 Pizza.prototype.getOnePizzaPrice = function(){
-var pizzaPrice = this.baseprice + this.getToppingCount() + this.size
-  return pizzaPrice; //amount of topings value plus amount of pizza
-  // amount of toppings + amountofpizza times baseprice + size
+  var pizzaPrice = this.baseprice + this.getToppingCount() + this.size
+  return pizzaPrice;
 }
 function Order(amount){
   this.pizzas = [];
@@ -24,42 +20,41 @@ function Order(amount){
     return totalprice;
   }
 }
-
 Order.prototype.addPizzas = function(pizza){
   this.pizzas.push(pizza)
 }
-Order.prototype.displayOrder = function(order){
-  // this.data = alldata
-return $("#totalPrice").text("$" + order)
+Order.prototype.displayOrder = function(){
+  var order;
+  this.pizzas.forEach(function(pizza){
+    $("#totalPrice").text(pizza.toppings)
+    console.log(pizza.toppings)
+    console.log(pizza.size)
+    console.log(pizza.getOnePizzaPrice())
+  })
+  console.log(order)
+  return order;
 }
 var newOrder = new Order();
 $(document).ready(function(){
   $("#formOne").submit(function(event){
-  event.preventDefault();
-  var toppingArray = [];
-  $(".toppings[name='topping']:checked").each(function(){
-    toppingArray.push($(this).val())
-    ;}
-  );
-  console.log(toppingArray)
-  // $('.toppings').each(function(topping) {
-  //   console.log(topping)
-  //   if ($(topping).is(':checked')){
-  //     console.log(topping)
-  //   }
-  //  });
-   console.log(toppingArray)
-  var amountGrabber = $("#amount").val(); //user inputted value
-  //user inputted check
-  var sizeGrabber = parseInt($("#size").val());// same thing
-  var newPizza = new Pizza(toppingArray,sizeGrabber)
-  newOrder.addPizzas(newPizza)
-  newOrder.displayOrder(newOrder.orderTotal());
-  console.log(newOrder)
-
-});
-$("#clearButton").click(function(event){
-  newOrder = new Order();
-  $("#totalPrice").text(" ")
-});
+    event.preventDefault();
+    var toppingArray = [];
+    $(".toppings[name='topping']:checked").each(function(){
+      toppingArray.push($(this).val())
+      ;}
+    );
+    console.log(toppingArray)
+    console.log(toppingArray)
+    var amountGrabber = $("#amount").val(); //user inputted value
+    //user inputted check
+    var sizeGrabber = parseInt($("#size").val());// same thing
+    var newPizza = new Pizza(toppingArray,sizeGrabber)
+    newOrder.addPizzas(newPizza)
+    newOrder.displayOrder();
+    console.log(newOrder.displayOrder())
+  });
+  $("#clearButton").click(function(event){
+    newOrder = new Order();
+    $("#totalPrice").text(" ")
+  });
 });
